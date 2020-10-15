27a4e8305fca310eae762d96704f7fd67e4ee49c
Suman Mitra <sumanmitra97@gmail.com>
2020-10-15 21:53:36 +0530

    dev complete phase 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ import { MoviedbService } from './moviedb.service';
 export class AppComponent {
   movies: MovieShortDescription[];
   details: MoveDetails;
+  cast: Cast[];
+  crew: Crew[];
+  similar: MovieShortDescription[];
 
   constructor(private _movidbService: MoviedbService) {}
 
@@ -43,49 +46,109 @@ export class AppComponent {
   showDescription(event): void {
     let selector = this.findAncestor(event.target, '.movie');
     if (selector) {
-      this._movidbService
-        .fetchMovieDetails(selector.lastChild.textContent)
-        .subscribe((res) => {
-          if (res) {
-            let genres: string[] = [];
-            res['genres'].forEach((item) => {
-              genres.push(item['name']);
-            });
-            let spoken_languages: string[] = [];
-            res['spoken_languages'].forEach((item) => {
-              spoken_languages.push(item['name']);
-            });
-            this.details = {
-              adult: res['adult'],
-              backdrop_path: res['backdrop_path']
-                ? `https://image.tmdb.org/t/p/w1280${res['backdrop_path']}`
-                : this._movidbService.backdrop_dummy,
-              budget: res['budget'],
-              genres: genres,
-              homepage: res['homepage']?res['homepage']:null,
-              imdb_id: `https://www.imdb.com/title/${res['imdb_id']}`,
-              original_language: res['original_language'],
-              original_title: res['original_title'],
-              overview: res['overview'],
-              poster_path: res['poster_path']
-                ? `https://image.tmdb.org/t/p/w185${res['poster_path']}`
-                : this._movidbService.poster_dummy,
-              release_date: new Date(res['release_date']).toDateString(),
-              runtime: `${res['runtime']} min`,
-              spoken_languages: spoken_languages,
-              status: res['status'],
-              vote_average: res['vote_average'],
-            };
-          }
-        });
-      window.scroll(0, 0);
+      let movieId: number = selector.lastChild.textContent;
+      this.fetchDetails(movieId);
     }
+  }
+
+  loadMovieDetails(event): void {
+    let selector = this.findAncestor(event.target, '.similar-movie');
+    if (selector) {
+      let movieId: number = selector.lastChild.textContent;
+      this.fetchDetails(movieId);
+    }
+  }
+
+  fetchDetails(movieId: number) {
+    this._movidbService.fetchMovieDetails(movieId).subscribe((res) => {
+      if (res) {
+        let genres: string[] = [];
+        res['genres'].forEach((item) => {
+          genres.push(item['name']);
+        });
+        let spoken_languages: string[] = [];
+        res['spoken_languages'].forEach((item) => {
+          spoken_languages.push(item['name']);
+        });
+        this.details = {
+          adult: res['adult'] < 18 ? '16+' : '18+',
+          backdrop_path: res['backdrop_path']
+            ? `https://image.tmdb.org/t/p/w1280${res['backdrop_path']}`
+            : this._movidbService.backdrop_dummy,
+          budget: res['budget'],
+          genres: genres,
+          homepage: res['homepage'] ? res['homepage'] : null,
+          imdb_id: `https://www.imdb.com/title/${res['imdb_id']}`,
+          original_title: res['original_title'],
+          overview: res['overview'],
+          poster_path: res['poster_path']
+            ? `https://image.tmdb.org/t/p/w185${res['poster_path']}`
+            : this._movidbService.poster_dummy,
+          release_date: new Date(res['release_date']).toDateString(),
+          runtime: `${res['runtime']} min`,
+          spoken_languages: spoken_languages,
+          status: res['status'],
+          vote_average: res['vote_average'],
+        };
+      }
+    });
+    this._movidbService.fetchMovieCredits(movieId).subscribe((res) => {
+      if (res) {
+        this.cast = [];
+        this.crew = [];
+        for (var index = 0; index < res['cast'].length; index++) {
+          if (index > 19) break;
+          let item = res['cast'][index];
+          let c: Cast = {
+            name: item['name'],
+            character: item['character'],
+            profile_path: item['profile_path']
+              ? `https://image.tmdb.org/t/p/w185${item['profile_path']}`
+              : this._movidbService.profile_dummy,
+          };
+          this.cast.push(c);
+        }
+        for (var index = 0; index < res['crew'].length; index++) {
+          if (index > 19) break;
+          let item = res['crew'][index];
+          let c: Crew = {
+            name: item['name'],
+            job: item['job'],
+            profile_path: item['profile_path']
+              ? `https://image.tmdb.org/t/p/w185${item['profile_path']}`
+              : this._movidbService.profile_dummy,
+          };
+          this.crew.push(c);
+        }
+      }
+    });
+    this._movidbService.fetchSimilarMovies(movieId).subscribe((res) => {
+      if (res) {
+        this.similar = [];
+        for (var index = 0; index < res['results'].length; index++) {
+          if (index > 19) break;
+          let item = res['results'][index];
+          let c: MovieShortDescription = {
+            poster_path: item['poster_path']
+              ? `https://image.tmdb.org/t/p/w185${item['poster_path']}`
+              : this._movidbService.poster_dummy,
+            original_title: item['original_title'],
+            vote_average: item['vote_average'],
+            release_date: new Date(item['release_date']).toDateString(),
+            id: item['id'],
+            overview: item['overview'],
+          };
+          this.similar.push(c);
+        }
+      }
+    });
+    window.scroll(0, 0);
   }
 
   findAncestor(el, sel) {
     while (
-      (el = el.parentElement) &&
-      !(el.matches || el.matchesSelector).call(el, sel)
+      !(el.matches || el.matchesSelector).call(el, sel) &&
+      (el = el.parentElement)
     );
     return el;
   }
@@ -101,13 +164,12 @@ export interface MovieShortDescription {
 }
 
 export interface MoveDetails {
-  adult: boolean;
+  adult: string;
   backdrop_path: string;
   budget: number;
   genres: string[];
   homepage: string;
   imdb_id: string;
-  original_language: string;
   original_title: string;
   overview: string;
   poster_path: string;
@@ -116,4 +178,16 @@ export interface MoveDetails {
   spoken_languages: string[];
   status: string;
   vote_average: number;
+}
+
+export interface Cast {
+  name: string;
+  character: string;
+  profile_path: string;
+}
+
+export interface Crew {
+  name: string;
+  job: string;
+  profile_path: string;
 }
