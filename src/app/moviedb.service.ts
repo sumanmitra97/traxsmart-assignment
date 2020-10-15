ec5c8778b7327f7fbe8792a1886721da821f6de5
Suman Mitra <sumanmitra97@gmail.com>
2020-10-16 00:08:11 +0530

    name changes

--- src/app/moviedb.service.ts
+++ src/app/moviedb.service.ts
@@ -9,11 +9,6 @@ export class MoviedbService {
   private API_KEY: string = 'api_key=e7a428d31ba9261aa8faf356856a54c8';
   constructor(private _httpClient: HttpClient) {}
 
-  getGenres() {
-    return this._httpClient.get(
-      `${this.API_URL}/genre/movie/list?${this.API_KEY}`
-    );
-  }
   fetchMovieDataByName(movieName: string) {
     return this._httpClient.get(
       `${this.API_URL}/search/movie?${this.API_KEY}&query=${movieName}`
